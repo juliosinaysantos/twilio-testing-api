44e95171da6f983285470da8a4a18df27e474e15
Julio Sinay <juliosinaysantos@outlook.com>
2021-08-23 16:01:50 -0600

    fix twilio callback message

--- api/twilio/callback.js
+++ api/twilio/callback.js
@@ -1,13 +1,11 @@
-const qs = require('querystring')
 const MessagingResponse = require('twilio').twiml.MessagingResponse
 
 module.exports = (req, res) => {
   const { body } = req
   console.log('New Twilio Webhook has been called', body)
 
-  const formValue = qs.parse(body)
   const twiml = new MessagingResponse()
-  twiml.message('You said: ' + formValue.Body)
+  twiml.message('You said: "' + body.Body + '"? 🤔')
 
   res.setHeader('Content-Type', 'text/xml')
   return res.send(twiml.toString(),)
