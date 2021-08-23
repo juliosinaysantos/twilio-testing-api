c08e2dfe13517ea599bc815dbb079b5e2c91caf9
Julio Sinay <juliosinaysantos@outlook.com>
2021-08-23 15:18:20 -0600

    setup vercel functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+
+  const [message, setMessage] = useState('')
+
+  const handlePingClick = async () => {
+    const response = await fetch('/api/ping')
+    const data = await response.json()
+    setMessage(data.message)
+  }
+
   return (
     <>
       <h1>Testing Twilio API and Twilio Webhooks</h1>
-      <button type="button">Let's ping</button>
+      <button type="button" onClick={handlePingClick}>Let's ping</button>
+      <p>{message}</p>
     </>
   )
 }
