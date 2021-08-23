b50a4070563dda2330c62f757cc3db23ae109c7b
Julio Sinay <juliosinaysantos@outlook.com>
2021-08-23 16:13:22 -0600

    play with waid

--- api/twilio/callback.js
+++ api/twilio/callback.js
@@ -4,8 +4,12 @@ module.exports = (req, res) => {
   const { body } = req
   console.log('New Twilio Webhook has been called', body)
 
+  const WAID = process.env.WAID
+
+  const isDeveloperWaId = body.WaId === WAID
+
   const twiml = new MessagingResponse()
-  twiml.message('You said: "' + body.Body + '"? 🤔')
+  twiml.message(`${isDeveloperWaId ? '🚀' : '👋'} You said: "${body.Body}"? 🤔`)
 
   res.setHeader('Content-Type', 'text/xml')
   return res.send(twiml.toString(),)
